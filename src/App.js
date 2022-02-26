1ffe7b48c79307d269b81ce2d5036222352d70e4
achalongithub <apturkarachal@gmail.com>
2022-02-26 18:38:37 +0530

    task2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
+import {useState} from 'react';
 import './App.css';
+import Box from './Card';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [userData, setUserData] = useState([]);
+  const [Buttonaction, setButtonAction,] = useState(false);
+  const handleClick = () => {
+    setButtonAction(true);
+    fetch('https://reqres.in/api/users?page=1')
+      .then((response) => response.json())
+      .then((res) => {
+        setUserData(res.data);
+        setInterval(() => {
+          setLoading(true);
+        }, 5000);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section>
+        <nav className="navdiv">
+          <div>
+             <h1 className='title'>UserCard</h1>
+             <button className='titlebts'onClick={handleClick}>
+                  Get Users
+                </button>
+          </div>         
+        </nav>
+
+        <div className="bodytextflex">
+          <div className="mainflex">
+            {Buttonaction ? (
+              loading ? (
+                <Box userData={userData} />
+              ) :
+               (
+                <div className="loader">
+                </div>
+              )
+            ) : (
+              <div  >
+                <p className="bodytext">Click on Get Users button to get users data</p>
+              </div>
+            )}
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
